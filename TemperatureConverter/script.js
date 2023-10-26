7e243a5d6471ae404fb3eedf304bedaef50424a3
Ritesh Sadh <sadhritesh32@gmail.com>
2023-10-26 22:35:03 +0530

    Project-1

---
+++ TemperatureConverter/script.js
@@ -0,0 +1,22 @@
+let cel = document.getElementsByClassName('inp')[0]
+let feh = document.getElementsByClassName('inp')[1]
+console.log(cel);
+console.log(feh);
+
+cel.addEventListener('input',function(){
+    let c = this.value; 
+    let f = (c * 9/5) + 32;
+    if (!Number.isInteger(f)){
+        f = f.toFixed(4);
+    }
+    feh.value = f;
+});
+
+feh.addEventListener('input',function(){
+    let f = this.value;
+    let c = (f - 32)*5/9;
+    if (!Number.isInteger(c)){
+        c = c.toFixed(4);
+    }
+    cel.value = c;
+});
